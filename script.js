1fb6f2f9d05aeeba6ff33c9f7c88f2df07de7e1e
Anmol <166452068+Anmol1l@users.noreply.github.com>
2024-10-13 22:02:47 +0530

    Create A Function playRound to Play Game Logic
    function compare human and computer choices and declare winner

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 function getComputerChoice (){
-    let computerChoice = Math.floor(Math.random()*3)+1
-    console.log (computerChoice)
-    if (computerChoice === 1 )
-        console.log ("rock")
-    else if (computerChoice === 2)
-        console.log ("paper")
-    else console.log("scissors")
+    let choiceRandomiser = Math.floor(Math.random()*3)+1
+    // console.log (choiceRandomiser)
+    if (choiceRandomiser === 1 )
+        return "rock";
+    else if (choiceRandomiser === 2)
+        return "paper";
+    else return "scissors";
 }
-getComputerChoice()
+// getComputerChoice()
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter rock, paper or scissors").toLowerCase()
-    if (humanChoice === "rock")
-        console.log("rock")
-    else if (humanChoice === "paper")
-        console.log("paper")
-    else if (humanChoice === "scissors")
-        console.log("scissors")
-    else console.log("Enter a valid input")
+    let humanInput = prompt("Enter rock, paper or scissors").toLowerCase()
+    if (humanInput === "rock")
+        return "rock";
+    else if (humanInput === "paper")
+        return "paper";
+    else if (humanInput === "scissors")
+        return "scissors";
+    else return "Please enter a Valid output"
 }
-getHumanChoice()
+// getHumanChoice()
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        console.log(`"TIE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`)
+    }
+    else if ((computerChoice === "rock" && humanChoice === "paper") || (computerChoice === "paper" && humanChoice === "scissors") || 
+    (computerChoice === "scissors" && humanChoice === "rock"))
+        console.log(`"YOU WIN! \n you choose ${humanChoice} and computer chose ${computerChoice}`)
+    else if ((humanChoice === "rock" && computerChoice === "paper")|| (humanChoice === "paper" && computerChoice === "scissors")|| 
+    (humanChoice === "scissors" && computerChoice === "rock")) {
+        console.log(`"YOU LOSE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`)
+    }
+    else {
+        console.log ("something broke Enter a valid output")
+    }
+}
+
+const computerSelection = getComputerChoice()
+const humanSelection = getHumanChoice()
+
+playRound (computerSelection, humanSelection)
