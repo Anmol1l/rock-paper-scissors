2f025a879aead81a3f619f3e4911c6b5b77798ed
Anmol <166452068+Anmol1l@users.noreply.github.com>
2024-10-14 18:23:35 +0530

    Add a Function to Play 5 rounds
    Function plays 5 rounds evaluate score and declare winner

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice (){
         return "paper";
     else return "scissors";
 }
-// getComputerChoice()
+
 
 function getHumanChoice() {
     let humanInput = prompt("Enter rock, paper or scissors").toLowerCase()
@@ -19,28 +19,51 @@ function getHumanChoice() {
         return "scissors";
     else return "Please enter a Valid output"
 }
-// getHumanChoice()
 
 let humanScore = 0
 let computerScore = 0
+
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === humanChoice) {
         console.log(`"TIE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`)
     }
     else if ((computerChoice === "rock" && humanChoice === "paper") || (computerChoice === "paper" && humanChoice === "scissors") || 
-    (computerChoice === "scissors" && humanChoice === "rock"))
+    (computerChoice === "scissors" && humanChoice === "rock")) {
         console.log(`"YOU WIN! \n you choose ${humanChoice} and computer chose ${computerChoice}`)
+        return "human-wins";
+    }
     else if ((humanChoice === "rock" && computerChoice === "paper")|| (humanChoice === "paper" && computerChoice === "scissors")|| 
     (humanChoice === "scissors" && computerChoice === "rock")) {
         console.log(`"YOU LOSE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`)
+        return "computer-wins"
     }
     else {
         console.log ("something broke Enter a valid output")
     }
 }
 
-const computerSelection = getComputerChoice()
-const humanSelection = getHumanChoice()
 
-playRound (computerSelection, humanSelection)
+function playGame() {
+    for(let i=0; i < 5; i++) {
+        const computerSelection = getComputerChoice()
+        const humanSelection = getHumanChoice()
+
+        let result = playRound (computerSelection,humanSelection)
+        if (result === "human-wins")
+            humanScore++;
+        else if (result === "computer-wins")
+            computerScore++;
+        console.log(`your score: ${humanScore} Computer score; ${computerScore}`)
+    }
+    if (humanScore < computerScore) {
+        console.log(`"Computer-Wins" Human Score ${humanScore} Computer Score ${computerScore}`)
+    }
+    else if (computerScore < humanScore) {
+        console.log(`Human-Wins Human Score ${humanScore} Computer Score ${computerScore}`)
+    }
+    else if (humanScore == computerScore) {
+        console.log (`Equal Score Human Score ${humanScore} Computer Score ${computerScore}`)
+    }
+}
+playGame()
