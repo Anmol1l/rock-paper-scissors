bbdf56d864720e497fce8256aa706bf240991ddf
Anmol <166452068+Anmol1l@users.noreply.github.com>
2025-09-04 03:00:00 +0530

    Add functions to track score and diplay results
    added function currentScore and trackScore to keep track of score and display results

--- script.js
+++ script.js
@@ -7,37 +7,60 @@ function getComputerChoice (){
         return "paper";
     else return "scissors";
 }
-let computerSelect = getComputerChoice()
+
+// take human input with buttons
 
 const rockBtn = document.querySelector("#rock")
 const paperBtn = document.querySelector("#paper")
 const scissorsBtn = document.querySelector("#scissors")
 
-rockBtn.addEventListener ('click' , function () {playRound(computerSelect,"rock")} )
-paperBtn.addEventListener ('click' , function () {playRound(computerSelect,"paper")} )
-scissorsBtn.addEventListener ('click' , function () {playRound(computerSelect,"scissors")} )
+rockBtn.addEventListener ('click' , function () {playRound(getComputerChoice(),"rock"), trackScore(), ScoreDisplay()} )
+paperBtn.addEventListener ('click' , function () {playRound(getComputerChoice(),"paper"), trackScore(), ScoreDisplay()} )
+scissorsBtn.addEventListener ('click' , function () {playRound(getComputerChoice(),"scissors"), trackScore(), ScoreDisplay()} )
 
+let result = document.querySelector(".result-window")
+result.style.whiteSpace = "pre-line";
 
-
-let humanScore = 0
-let computerScore = 0
 
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === humanChoice) {
-        console.log(`"TIE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`)
+        result.textContent = `"TIE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`
     }
     else if ((computerChoice === "rock" && humanChoice === "paper") || (computerChoice === "paper" && humanChoice === "scissors") || 
     (computerChoice === "scissors" && humanChoice === "rock")) {
-        console.log(`"YOU WIN! \n you choose ${humanChoice} and computer choose ${computerChoice}`)
+        humanScore ++ ;
+        result.textContent = `"YOU WIN! \n you choose ${humanChoice} and computer choose ${computerChoice}`
         return "human-wins";
     }
     else if ((humanChoice === "rock" && computerChoice === "paper")|| (humanChoice === "paper" && computerChoice === "scissors")|| 
     (humanChoice === "scissors" && computerChoice === "rock")) {
-        console.log(`"YOU LOSE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`)
+        computerScore ++ ;
+        result.textContent = `"YOU LOSE!" \n you choose ${humanChoice} and computer choose ${computerChoice}`
         return "computer-wins"
     }
     else {
         console.log ("something broke Enter a valid input")
     }
+}
+
+let humanScore = 0
+let computerScore = 0
+
+let finalScore = document.querySelector(".final-score")
+let currentScore = document.querySelector(".current-score")
+currentScore.style.whiteSpace = "pre-line";
+
+function trackScore() {
+    if (humanScore === 5){
+        finalScore.textContent = "Human Wins"
+    }
+    else if (computerScore === 5) {
+        finalScore.textContent = "Computer Wins"
+    }
+}
+function ScoreDisplay() {
+    currentScore.textContent = `Computer Score: ${computerScore}
+    Human Score: ${humanScore}`
+
 }
